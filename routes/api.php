ac7b0383ea0d451e206fcd79a95bfa06ae6aae23
RYN BSD <rynbsd04@gmail.com>
2024-11-27 17:05:19 +0100

    test email

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Mail\Mailable;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get("/test-email", function () {
+    $mailable = new Mailable();
+
+    $mailable
+        ->from('rynbsd04@gmail.com')
+        ->to('bbfgdh@gmail.com')
+        ->subject('test subject')
+        ->html('my first message');
+
+    $result = Mail::send($mailable);
+
+    return response(["ok" => true], 200);
+});
